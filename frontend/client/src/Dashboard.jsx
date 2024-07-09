fe66af37625774894846d1f521481fa9223440aa
RadhikaSharma777 <144764170+RadhikaSharma777@users.noreply.github.com>
2024-07-09 16:58:31 +0530

    Updated Dashboard.jsx by Radhika Sharma
    This file contains a Dashboard page setup which normal users can access after login.

--- frontend/client/src/Dashboard.jsx
+++ frontend/client/src/Dashboard.jsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React from 'react'
 
 function Dashboard() {
     return(
-        <h2>Dashboard Component</h2>
+        <h2>Dashboard Component for user</h2>
     )
 }
 
