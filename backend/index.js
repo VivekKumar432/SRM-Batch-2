36331bf34ab7f8c650bc3320533631cbdd6484c0
RadhikaSharma777 <radhikasharma5047@gmail.com>
2024-07-21 18:04:48 +0530

    Dockerization by Radhika (11722210006)

--- backend/index.js
+++ backend/index.js
@@ -19,6 +19,10 @@ app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use('/api/admin', adminRoutes);
 
+app.get('/',(req,res) => {
+    res.send('Server is running!');
+});
+
 
 const port = process.env.PORT || 4545;
 app.listen(port, console.log(`Listening on port ${port}...`));
