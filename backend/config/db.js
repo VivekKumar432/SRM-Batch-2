9e39f7d5c1bdc2aecd2488460d1f435f39c6ab46
harpreet kaur <harpreetkour1921@gmail.com>
2024-07-22 15:32:47 +0530

    docker file added

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 module.exports = () => {
   try {
-    mongoose.connect(process.env.DB);
+    mongoose.connect(process.env.MONGO_URI);
     console.log("Connected to database successfully");
   } catch (error) {
     console.log(error);
--- backend/index.js
+++ backend/index.js
@@ -19,6 +19,9 @@ app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use('/api/admin', adminRoutes);
 
+app.get('/',(req,res) => {
+    res.send('hello,world!');
+});
 
 const port = process.env.PORT || 4545;
 app.listen(port, console.log(`Listening on port ${port}...`));
