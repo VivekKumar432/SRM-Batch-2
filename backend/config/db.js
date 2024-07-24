eba4629f45ebfc06ab05100cc880486eca7b8475
Harpreet1423 <harpreetkour1921@gmail.com>
2024-07-25 00:11:01 +0530

    hide credentials

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 module.exports = () => {
   try {
-    mongoose.connect(process.env.MONGO_URI);
+    mongoose.connect(process.env.DB);
     console.log("Connected to database successfully");
   } catch (error) {
     console.log(error);
