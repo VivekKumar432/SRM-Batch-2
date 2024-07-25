5e9e65cea7754f7154225a1e3fbf8c95542d7756
hrsh-1294 <harshvashishth12@gmail.com>
2024-07-25 19:05:10 +0530

    added .gitignore file

--- backend/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { createUser, loginUser } = require("../controllers/userController");
-const userRouter = express.Router();
-
-userRouter.post("/create-user", createUser);
-userRouter.post("/login-user", loginUser);
-
-module.exports = userRouter;
