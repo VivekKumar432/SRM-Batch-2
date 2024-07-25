aa46206e2eedb99720bf93e89206967165b9fb36
Vivek Kumar <piebytwo014@gmail.com>
2024-07-25 18:51:41 +0530

    .

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import axios from "axios";
+import axios from "axios"; 
 import { Link, useNavigate } from "react-router-dom";
 import "../assets/CSS/loginPage.css";
  
